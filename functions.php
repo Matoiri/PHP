5bb605d141a502c54fb62ecd637b993d8c52610f
el-amine <matoirifr@gmail.com>
2024-06-05 15:12:04 +0200

    second commit

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function displayAuthor( $authorEmail, $users){
+
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+        }
+    }
+}
+
+function isValidRecipe($recipe){
+
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+
+    return $isEnabled;
+}
+
+function getRecipes($recipes){
+
+    $valid_recipes = [];
+
+    foreach ($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $valid_recipes[] = $recipe;
+        }
+    }
+
+    return $valid_recipes;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,88 +1,23 @@
 <?php
-$users = [
-        [
-            'full_name' => 'Mickaël Andrieu',
-            'email' => 'mickael.andrieu@exemple.com',
-            'age' => 34,
-        ],
-        [
-            'full_name' => 'Mathieu Nebra',
-            'email' => 'mathieu.nebra@exemple.com',
-            'age' => 34,
-        ],
-        [
-            'full_name' => 'Laurène Castor',
-            'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-        ],
-    ];
 
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => 'Etape 1 : des flageolets !',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => 'Etape 1 : de la semoule',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => 'Etape 1 : prenez une belle escalope',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-];
+require_once(__DIR__ . '/variables.php');
+require_once(__DIR__.'/functions.php');
 
-function displayAuthor( $authorEmail, $users){
-
-    foreach ($users as $user) {
-        if ($authorEmail === $user['email']) {
-            return $user['full_name'] . '(' . $user['age'] . ' ans)';
-        }
-    }
-}
-
-function isValidRecipe($recipe){
-
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-function getRecipes($recipes){
-    
-    $valid_recipes = [];
-
-    foreach ($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $valid_recipes[] = $recipe;
-        }
-    }
-
-    return $valid_recipes;
-}
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Site de recettes - page d'accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-<h1>Affichage des recettes</h1>
-    <div>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+    <?php include_once(__DIR__.'/header.php');?>
+        <h1>Affichage des recettes</h1>
         <?php $number= 1; foreach(getRecipes($recipes) as $recipe): ?>
 
         <article>
@@ -94,6 +29,6 @@ function getRecipes($recipes){
         <?php endforeach?>
     </div>
 
-            
+    <?php require_once(__DIR__ . '/footer.php'); ?> 
 </body>
 </html>
